64ea96f177f79a62ea92d297bdf57db46084d8b7
Efoe Max <efoemaxi17@gmail.com>
2025-03-21 18:35:51 +0100

    signin improved

--- SignUp.js
+++ SignUp.js
@@ -1,21 +1,30 @@
 "use strict";
- let passOne = document.querySelector("#pass1");
-  let passTwo = document.querySelector("#pass2");
-  let userName = document.querySelector("#fname");
-  let userEmail = document.querySelector("#email")
-let btn = document.querySelector(".btn");
+let passOne = document.querySelector("#pass1");
+let passTwo = document.querySelector("#pass2");
+let userName = document.querySelector("#fname");
+let userEmail = document.querySelector("#email");
+let btn = document.querySelector("button");
 
 btn.addEventListener("click", (e) => {
- 
-//   console.log(passOne.value, passTwo.value);
+  //   console.log(passOne.value, passTwo.value);
+
+  if (
+    (passOne.value ==="") ||
+    (passTwo.value ==="") ||
+    (userEmail.value ==="") ||
+    (userName.value ==="")
+  ) {
+    alert("Error: Ensure you fill all fields");
+    e.preventDefault();
+    userName.focus();
+  }
   if (passOne.value !== passTwo.value) {
-    alert("Incorrect Details");
+    alert("Incorrect Password Details");
     e.preventDefault();
     passOne.focus();
-  }else{
+  } else {
     localStorage.setItem("Username", userName.value);
     localStorage.setItem("Email", userEmail.value);
     localStorage.setItem("Password", passOne.value);
   }
-
 });
