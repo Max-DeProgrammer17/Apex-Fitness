4d907f4b7c58ffb63e2754755c731f39e2a251f5
Efoe Max <efoemaxi17@gmail.com>
2025-03-21 19:26:35 +0100

    working on store

--- store.js
+++ store.js
@@ -12,12 +12,25 @@ closer.addEventListener("click", () => {
   menu.style.display = "none";
 });
 
+
+let arr = [];
+
 //shopping cart icon
 let shopcount = document.querySelector(".count");
 let btns = document.querySelectorAll(".btnn");
 btns.forEach((btn) => {
     btn.addEventListener("click", ()=>{
         shopcount.innerHTML++;
+        let cart = btn.closest(".cart");
+      let nname = cart.querySelector("h2").textContent;
+      let price = cart.querySelector("h3").textContent;
+
+      console.log( nname, price);
+
+        
     })
-})
-console.log(btns);
+});
+
+
+
+console.log(cart);
